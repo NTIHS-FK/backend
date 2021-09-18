cffa994fed23dd43568d93b8eb296b3c8fe3a4d5
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-19 00:27:32 +0800

    run command add restart parameter

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-docker run --name pgdb -e POSTGRES_USER=pguser -e POSTGRES_PASSWORD=4818115 -e POSTGRES_DB=ntihsFK -p 5432:5432 -d postgres
+docker run --name pgdb -e POSTGRES_USER=pguser -e POSTGRES_PASSWORD=4818115 -e POSTGRES_DB=ntihsFK -p 5432:5432 -d postgres --restart always
