53f70f122f2612a75e5b39cd4a807e975f8ff11f
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-18 20:47:03 +0800

    init

---
+++ src/Application.kt
@@ -0,0 +1,46 @@
+package com.ntihs_fk
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.sessions.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.auth.*
+import io.ktor.gson.*
+import io.ktor.client.*
+import twitter4j.StatusUpdate
+import twitter4j.TwitterFactory
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Sessions) {
+    }
+
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+        get("/") {
+            val twitter = TwitterFactory.getSingleton()
+            val statusUpdate = StatusUpdate("a")
+            twitter.updateStatus(statusUpdate)
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+    }
+}
