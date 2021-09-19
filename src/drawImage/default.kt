0158733010ff2606b9173ba5a10181cc3ec272b5
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-19 19:43:08 +0800

    消除字體鋸齒

--- src/drawImage/default.kt
+++ src/drawImage/default.kt
@@ -3,6 +3,8 @@ package com.ntihs_fk.drawImage
 import com.ntihs_fk.functions.randomString
 import java.awt.Color
 import java.awt.Font
+import java.awt.GraphicsEnvironment
+import java.awt.RenderingHints
 import java.awt.image.BufferedImage;
 import java.io.File
 import java.util.*
@@ -18,12 +20,18 @@ fun defaultDraw(text: String): String {
     val image = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
     val g2d = image.createGraphics()
     val fileName = Date().time.toString() + randomString()
+    val ge = GraphicsEnvironment.getLocalGraphicsEnvironment()
     val font = Font.createFont(
-        Font.TRUETYPE_FONT,
-        File("./sarasa-mono-sc-light.ttf")
-    ).deriveFont(Font.PLAIN).deriveFont(fontSize)
-
+        Font.PLAIN,
+        File("./sarasa-mono-sc-bolditalic.ttf")
+    ).deriveFont(fontSize)
+    // 消除字體鋸齒
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON)
+    g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL,RenderingHints.VALUE_STROKE_DEFAULT)
+    // 載入字體
+    ge.registerFont(font)
     g2d.font = font
+    // 背景顏色
     g2d.background = Color.black
     g2d.clearRect(0, 0, width, height)
     drawString(g2d, top, left, linesData)
