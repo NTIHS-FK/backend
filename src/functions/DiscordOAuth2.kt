b9246d82092a5dc78d99f90c5657d645c689f1d6
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-23 22:45:18 +0800

    鬼才Discord OAuth2

--- src/functions/DiscordOAuth2.kt
+++ src/functions/DiscordOAuth2.kt
@@ -3,8 +3,6 @@ package com.ntihs_fk.functions
 import com.github.kevinsawicki.http.HttpRequest
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import com.ntihs_fk.functions.DiscordOAuth2.Companion.serializeToMap
-import io.ktor.features.*
 
 class DiscordOAuth2 {
     companion object {
@@ -21,6 +19,14 @@ class DiscordOAuth2 {
             val refresh_token: String? = null
         )
 
+        data class AccessTokenResponse(
+            val access_token: String,
+            val token_type: String,
+            val expires_in: Long,
+            val refresh_token: String,
+            val scope: String
+        )
+
         private inline fun <I, reified O> I.convert(): O {
             val json = gson.toJson(this)
             return gson.fromJson(json, object : TypeToken<O>() {}.type)
@@ -28,12 +34,14 @@ class DiscordOAuth2 {
 
         private fun <T> T.serializeToMap(): Map<String, Any> = convert()
 
-        private fun requestForm(formData: Map<String, Any>): String =
-            HttpRequest.post("$discordAPIUrl/oauth2/token")
+        private fun requestForm(formData: Map<String, Any>): AccessTokenResponse {
+            val response = HttpRequest.post("$discordAPIUrl/oauth2/token")
                 .form(formData)
-                .body()
+            response.ok()
+            return gson.fromJson(response.body(), AccessTokenResponse::class.java)
+        }
 
-        fun exchange_code(code: String, redirect_uri: String) {
+        fun exchange_code(code: String, redirect_uri: String): AccessTokenResponse {
             val data = Data(
                 client_id = discordConfig.discord_id,
                 client_secret = discordConfig.discord_secret,
@@ -42,10 +50,10 @@ class DiscordOAuth2 {
                 redirect_uri = redirect_uri
             )
 
-            requestForm(data.serializeToMap())
+            return requestForm(data.serializeToMap())
         }
 
-        fun refresh_token(refresh_token: String) {
+        fun refresh_token(refresh_token: String): AccessTokenResponse {
             val data = gson.toJson(
                 Data(
                     client_id = discordConfig.discord_id,
@@ -55,7 +63,7 @@ class DiscordOAuth2 {
                 )
             )
 
-            requestForm(data.serializeToMap())
+            return requestForm(data.serializeToMap())
         }
     }
 }
