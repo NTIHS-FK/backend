be8e9da3824279b317b323e4735530ddbad454c8
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-25 20:33:07 +0800

    expires at 7 days

--- src/functions/Config.kt
+++ src/functions/Config.kt
@@ -13,9 +13,11 @@ class Config {
         val discordConfig: DiscordConfig = Gson().fromJson(discordConfigJSONString, DiscordConfig::class.java)
         val secret = System.getenv("jwt_secret") ?: "secret"
 
-        private const val domain = "127.0.0.1:8080"
-        private const val ssl = false
+        private val domain = System.getenv("domain") ?: "127.0.0.1:8080"
+        private val ssl = System.getenv("ssl").toBoolean()
+
         val issuer = "http${if (ssl) "s" else ""}://$domain"
         val audience = "$issuer/vote"
+        val expiresAt = 60000 * 60 * 24 * 7
     }
 }
