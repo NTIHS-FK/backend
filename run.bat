76da9c5c6e2f1539e0dad6be0abd6d93d75562d5
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-25 21:27:33 +0800

    idk

--- run.bat
+++ run.bat
@@ -1,3 +1,8 @@
-docker run --name pgdb -e POSTGRES_USER=pguser -e POSTGRES_PASSWORD=4818115 -p 5432:5432 -d postgres --restart always
+docker run --name pgdb\
+-e POSTGRES_USER=pguser\
+-e POSTGRES_PASSWORD=4818115\
+-e domain=127.0.0.1:8080\
+-e ssl=true\
+-p 5432:5432 -d postgres --restart always
 
 @REM JAR 參數 -DLOG_DEST=./logs -DLOG_MAX_HISTORY=1
