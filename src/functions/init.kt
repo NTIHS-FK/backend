ba06ef3c64a01e8c538e223e859419fca89ae984
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-29 22:40:27 +0800

    idk

--- src/functions/init.kt
+++ src/functions/init.kt
@@ -1,15 +1,31 @@
 package com.ntihs_fk.functions
 
+import com.google.gson.Gson
+import com.ntihs_fk.data.TwitterConfig
 import java.io.File
 import org.slf4j.Logger
 
 fun init(log: Logger) {
     log.info("Init directory")
+
+    val gson = Gson()
     val image = File("./image")
     val textImage = File("./textImage")
+    val twitterConfigFIle = File("./Twitter.config.json")
 
-    if (!image.exists()  && !image.isDirectory)
+    if (!image.exists() && !image.isDirectory)
         image.mkdir()
     if (!textImage.exists() && !textImage.isDirectory)
         textImage.mkdir()
+    if (!twitterConfigFIle.exists() && !twitterConfigFIle.isFile)
+        twitterConfigFIle.writeText(
+            gson.toJson(
+                TwitterConfig(
+                    "",
+                    "",
+                    "",
+                    ""
+                )
+            )
+        )
 }
