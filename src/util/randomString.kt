cb3a88612a16536b7002dd9b1cdf25b20a9735b2
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-10-04 23:14:51 +0800

    idk

--- src/util/randomString.kt
+++ src/util/randomString.kt
@@ -2,7 +2,6 @@ package com.ntihs_fk.util
 
 import kotlin.random.Random
 
-
 fun randomString(count: Int): String {
     val chars = ('0'..'9') + ('a'..'z') + ('A'..'Z')
     return (0..count).map {
